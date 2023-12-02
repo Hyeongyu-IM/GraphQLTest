5640df092445c4c45c0b731fda83f2b54b5c3a4e
HyeonGyuIM <limku220@gmail.com>
2023-12-03 00:07:24 +0900

    [feat] AppIcon, ProductName 추가

--- Plugin/EnvironmentPlugin/ProjectDescriptionHelpers/InfoPlistValue.swift
+++ Plugin/EnvironmentPlugin/ProjectDescriptionHelpers/InfoPlistValue.swift
@@ -8,6 +8,7 @@
 import ProjectDescription
 
 public let infoPlistValues: [String: InfoPlist.Value] = [
+    "CFBundleName": "마미톡쇼핑몰",
     "UILaunchStoryboardName": "LaunchScreen",
     "UIApplicationSceneManifest": [
         "UIApplicationSupportsMultipleScenes": false,
